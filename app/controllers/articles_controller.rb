aae034cd1a5679088f206f08ddce491e35e9352e
Emma Drueke <emma.drueke@gmaila.com>
2018-09-28 20:19:37 -0500

    Corrected typo in config/routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,19 +2,19 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def new
   end
 
-  def create
-    @article = Article.new(article_params)
+def create
+  @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+  @article.save
+  redirect_to @article
+end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
-
-  private
-    def article_params
-      params.require(:article).permit(:title, :text)
-    end
 end
